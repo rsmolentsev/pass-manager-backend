e0151965d064061cb56d7cd99058067ad9c0c468
Roman Smolentsev <smolentsev.work@gmail.com>
2025-04-20 21:58:50 +0300

    delete: run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JWT_SECRET="your-secret-key-here"
-export DB_URL="jdbc:postgresql://localhost:5432/pass_manager"
-export DB_USER="pass_manager"
-export DB_PASSWORD="pass_manager"
-
-./gradlew run 
